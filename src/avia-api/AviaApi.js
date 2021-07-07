52138f74acedb4dd8a110f5157ea3fbebcdf37b6
Anton <churshukovanton@gmail.com>
2021-07-07 20:26:05 +0300

    commit

--- src/avia-api/AviaApi.js
+++ src/avia-api/AviaApi.js
@@ -1,27 +1,23 @@
+import axios from 'axios';
 
 class AviaApi {
-
   searchId = null;
 
   async getIdAndTickets() {
-    let tickets
-    if(this.searchId) {tickets = await this.getTickets();
-    return tickets}
-    const response = await fetch(`https://front-test.beta.aviasales.ru/search`);
-    const res = await response.json()
-    const id = res.searchId
-    this.searchId = id
-    tickets = this.getTickets()
-    return tickets    
+    if (this.searchId) {
+      const tickets = await this.getTickets();
+      return tickets;
+    }
+    const {data:{searchId}} = await axios.get(`https://front-test.beta.aviasales.ru/search`);
+    this.searchId = searchId;
+    const tickets = await this.getTickets();
+    return tickets;
   }
 
   async getTickets() {
-    const response = await fetch(`https://front-test.beta.aviasales.ru/tickets?searchId=${this.searchId}`);
-    if(response.status===500) throw new Error('500err');
-    if (!response.ok) throw new Error('404err');
-    const tickets = await response.json();
-    return tickets
+    const data = await axios.get(`https://front-test.beta.aviasales.ru/tickets?searchId=${this.searchId}`);
+    return data.data;
   }
 }
 
-export default AviaApi
+export default AviaApi;
--- src/store/actions/getTicketsAction.js
+++ src/store/actions/getTicketsAction.js
@@ -6,21 +6,19 @@ const aviaApi = new AviaApi();
 const getTicketsStarted = () => ({ type: GET_TICKETS_STARTED });
 const getTicketsCompleted = (tickets) => ({ type: GET_TICKETS_COMPLETED, tickets });
 const endFetch = () => ({ type: FETCH_END });
-const getTickets = () => (dispatch) => {
+const getTickets = () => async (dispatch) => {
   dispatch(getTicketsStarted());
-  aviaApi
-    .getIdAndTickets()
-    .then(({ tickets, stop }) => {
-      dispatch(getTicketsCompleted(tickets));
-      if (!stop) dispatch(getTickets());
-      else {
-        dispatch(endFetch());
-      }
-    })
-    .catch((err) => {
-      if (err.message === '404err') return;
-      if (err.message === '500err') dispatch(getTickets());
-    });
+  try {
+    const { tickets, stop } = await aviaApi.getIdAndTickets();
+    dispatch(getTicketsCompleted(tickets));
+    if (!stop) dispatch(getTickets());
+    else {
+      dispatch(endFetch());
+    }
+  } catch (err) {
+    if (err.response.status === 404) return;
+    if (err.response.status === 500) dispatch(getTickets());
+  }
 };
 
 export default getTickets;
